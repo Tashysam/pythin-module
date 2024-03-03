4b43ca0c4009172f4f2df37d49ee3e0eee6a2ff5
tashysam <tashysam6@gmail.com>
2024-03-03 23:34:44 +0300

    dart

---
+++ README.md
@@ -0,0 +1,2 @@
+#read me 
+#unlocking new skills
---
+++ main.dart
@@ -0,0 +1,6 @@
+void main() {
+  print("Hello world!");
+  print("Welcome to dat learning program");
+  print("Getting into tech is the best decision I've ever made");
+  print("Now print") ;
+}
